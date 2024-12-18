08999a9d80a2606d50ff5abd07a3dcfe0deabf72
datbt-2563 <bui.thanh.dat@sun-asterisk.com>
2024-12-18 11:07:16 +0700

    Make dbUser optional in ClusterConfig and update database name for dev-cluster

--- src/configuration/redshift-cluster.ts
+++ src/configuration/redshift-cluster.ts
@@ -3,7 +3,7 @@ export interface ClusterConfig {
   region: string;
   clusterIdentifier: string;
   database: string;
-  dbUser: string;
+  dbUser?: string;
 }
 
 export const clusters: ClusterConfig[] = [
@@ -11,8 +11,8 @@ export const clusters: ClusterConfig[] = [
     clusterName: "dev-cluster",
     region: "ap-northeast-1",
     clusterIdentifier: "dev-coupon-redshift-cluster",
-    database: "dev",
-    dbUser: "admin",
+    database: "prd_coupon_logs_db",
+    // dbUser: "admin",
   },
 ];
 
