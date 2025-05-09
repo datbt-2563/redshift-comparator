4fb635e9d506ac6bf1fd8554b5b9971880a75100
pvHung-nopro <pham.van.hung-b@sun-asterisk.com>
2025-05-09 15:50:58 +0700

    Update campaignId in showMenu function

--- index.ts
+++ index.ts
@@ -62,8 +62,8 @@ const showMenu = async () => {
     case "run-all-queries-3-times":
       console.log("Running all queries 3 times...");
       const clusterName0 = await askForClusterName();
-      for (let i = 0; i < 2; i++) {
-        const campaignId = `new_query_official_${clusterName0}_run_${i + 1}`;
+      for (let i = 0; i < 1; i++) {
+        const campaignId = `new_query_2_official_${clusterName0}_run_${i + 1}`;
         await runAllQueries(clusterName0, `Run ${i + 1}/3 times`, campaignId);
       }
       break;
