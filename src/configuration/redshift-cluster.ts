e261b25f0b6df007cda6396921b6ddbe4386725c
pvHung-nopro <pham.van.hung-b@sun-asterisk.com>
2025-05-19 11:02:55 +0700

    Update Redshift cluster identifier for dev environment

--- src/configuration/redshift-cluster.ts
+++ src/configuration/redshift-cluster.ts
@@ -11,10 +11,10 @@ export const clusters: ClusterConfig[] = [
   {
     clusterName: "ra3.large_x3nodes",
     region: "ap-northeast-1",
-    clusterIdentifier: "dev-coupon-redshift-cluster-1",
+    clusterIdentifier: "dev-coupon-redshift-cluster-2",
     database: "prd_coupon_logs_db",
     adminPasswordArn:
-      "arn:aws:secretsmanager:ap-northeast-1:856562439801:secret:redshift!dev-coupon-redshift-cluster-1-admin-BUjOEm",
+      "arn:aws:secretsmanager:ap-northeast-1:856562439801:secret:redshift!dev-coupon-redshift-cluster-2-admin-MACdHt",
   },
 ];
 
